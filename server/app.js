e2abdc2011e13a3a6ef883980c71817ebbf8bcce
VedNik3 <vedantnikam812@gmail.com>
2025-04-30 13:44:59 +0530

    changes

--- server/app.js
+++ server/app.js
@@ -69,7 +69,7 @@ app.use(cookieParser());
 
 const corsOptions = {
   origin: [
-   'https://car-rental-sooty-three.vercel.app', 
+   'car-rental-git-vedant-vedant-nikams-projects.vercel.app', 
    'http://localhost:5173'  // Keep local development URL
  ], 
  credentials: true,
